95a7a744c67b3312a845df67d35b8434e41653e1
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-05-26 18:24:36 -0300

    "update"

--- app.py
+++ app.py
@@ -7,6 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dash():
+    return render_template('dashboard.html')
+
+@app.route('/tables')
+def table():
+    return render_template('tables.html')
+
+@app.route('/billing')
+def billing():
+    return render_template('billing.html')
+
+@app.route('/virtual-reality')
+def virtual():
+    return render_template('virtual-reality.html')
+
+@app.route('/rtl')
+def rtl():
+    return render_template('rtl.html')
+
+@app.route('/notifications')
+def notification():
+    return render_template('notifications.html')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
